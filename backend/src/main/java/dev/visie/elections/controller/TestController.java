029dea4896f10659bac4cff1d0d4478427a45a17
Aaron2404 <70571116+Aaron2404@users.noreply.github.com>
2024-09-18 10:27:02 +0200

    Added: swagger.

--- backend/src/main/java/dev/visie/elections/controller/TestController.java
+++ backend/src/main/java/dev/visie/elections/controller/TestController.java
@@ -1,5 +1,6 @@
 package dev.visie.elections.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/")
+    @Operation(summary = "Test endpoint")
     public String index() {
         return "Test";
     }
