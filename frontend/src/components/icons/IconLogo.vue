e3d91d3a9270afedd7093b17599174fa5a4eb271
jasper van groeningen <jasper.van.groeningen@hva.nl>
2024-09-25 09:51:17 +0200

    added: styling and logo

---
+++ frontend/src/components/icons/IconLogo.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps<{
+  width: string;
+  height: string;
+}>();
+</script>
+
+<template>
+  <section :class="`${width} ${height} flex items-center`">
+    <img alt="logo" src='../../../public/img.png'>
+  </section>
+
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/components/nav/MainNavbar.vue
+++ frontend/src/components/nav/MainNavbar.vue
@@ -1,18 +1,21 @@
 <script setup lang="ts">
 
 
+import IconLogo from "@/components/icons/IconLogo.vue";
 </script>
 
 <template>
-  <nav class="">
-    <ul class="flex items-center">
-      <li class="text-red-800"><router-link to="/">Home</router-link></li>
-      <li><router-link to="/about">About</router-link></li>
-      <li><router-link to="/parties">Parties</router-link></li>
-      <li><router-link to="/forum">Forum</router-link></li>
-      <li><router-link to="/login">Log in</router-link></li>
-
+  <nav class="w-full gap-6 bg-NavBlue m-0 flex items-center h-20">
+    <section class="ml-4">
+      <icon-logo height="h-20" width="w-32"/>
+    </section>
+    <ul class="flex ml-4 list-none gap-6 text-2xl font-bold flex-grow">
+      <li><router-link class="text-white" to="/">Home</router-link></li>
+      <li><router-link class="text-white" to="/about">About</router-link></li>
+      <li><router-link class="text-white" to="/parties">Parties</router-link></li>
+      <li><router-link class="text-white" to="/forum">Forum</router-link></li>
     </ul>
+    <router-link class="text-white text-2xl font-bold mr-6" to="/login">Login</router-link>
   </nav>
 </template>
 
