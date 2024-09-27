3a8f59a745f19a021a53ebe86d57fbaf461b6893
jasper van groeningen <jasper.van.groeningen@hva.nl>
2024-09-27 13:48:08 +0200

    Added: Footer and positioned footer at bottom

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import MainNavbar from "@/components/nav/MainNavbar.vue";
+import MainFooter from "@/components/footer/MainFooter.vue";
 </script>
 
 <template>
+
   <main-navbar />
-  <br />
-  <RouterView />
+  <main class="p-5">
+    <RouterView />
+  </main>
+  <MainFooter />
+
 </template>
 
 <style scoped>
+main{
+  flex: 1;
+}
 
 </style>
---
+++ frontend/src/components/footer/MainFooter.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <footer>
+      this is the footer.
+  </footer>
+
+</template>
