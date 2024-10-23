30038d0fa2834baa43cb5c6723db6c6f4af737d3
Aaron2404 <70571116+Aaron2404@users.noreply.github.com>
2024-10-23 10:04:48 +0200

    Added: prime vue.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,13 +2,14 @@
 import { RouterLink, RouterView } from 'vue-router'
 import MainNavbar from "@/components/nav/MainNavbar.vue";
 import MainFooter from "@/components/footer/MainFooter.vue";
+import Toast from 'primevue/toast';
+
 </script>
-
 <template>
-
   <main-navbar />
   <main>
     <RouterView />
+    <Toast />
   </main>
   <MainFooter />
 
