6bafa8d4f0282452102a4a24525f92d89702ff66
Esa <esa03rottier@gmail.com>
2024-11-12 13:28:00 +0100

    colour per party added

---
+++ frontend/src/components/map/partyColors.js
@@ -0,0 +1,26 @@
+export default {
+    VVD: '#FF9900',                // People's Party for Freedom and Democracy
+    CDA: '#00A651',                // Christian Democratic Appeal
+    D66: '#009688',                // Democrats 66
+    PVV: '#1D4E89',                // Party for Freedom
+    PvdA: '#EB1C24',               // Labour Party
+    GroenLinks: '#83C348',         // GreenLeft
+    SP: '#EF3340',                 // Socialist Party
+    ChristenUnie: '#003399',       // Christian Union
+    SGP: '#FF6600',                // Reformed Political Party
+    PartijvoordeDieren: '#66CC33', // Party for the Animals
+    Volt: '#68217A',               // Volt Netherlands
+    JA21: '#FFD700',               // Right-wing political party JA21
+    FVD: '#800000',                // Forum for Democracy
+    DENK: '#00ADEF',               // DENK
+    BBB: '#00A550',                // Farmer–Citizen Movement (BoerBurgerBeweging)
+    BIJ1: '#FF0099',               // BIJ1
+    VijftigPLUS: '#6A0DAD',        // 50PLUS Party
+    Piratenpartij: '#5B5BE2',      // Pirate Party
+    LP: '#FFD700',                 // Libertarian Party
+    NLBeter: '#009999',            // NLBeter
+    Splinter: '#FF4500',           // Splinter Party
+    JezusLeeft: '#FFB6C1',         // Jesus Lives Party
+    CodeOranje: '#FFA500',         // Code Orange
+    VNL: '#00008B',                // For the Netherlands (VoorNederland)
+};
